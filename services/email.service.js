074159e47f8379349b85b60adb6f0b7e87656d53
AnkeetSah <ankeetkumar7777@gmail.com>
2025-10-08 16:33:10 +0530

    solved tghhe email issue

--- services/email.service.js
+++ services/email.service.js
@@ -1,4 +1,3 @@
-// services/email.service.js
 import { Resend } from "resend";
 import ejs from "ejs";
 import path from "path";
@@ -11,26 +10,24 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Initialize Resend with API key
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendCredentialsEmail = async ({ email, name, password }) => {
   try {
-    // Render EJS template
     const htmlContent = await ejs.renderFile(
       path.resolve(__dirname, '../emails/templates/credentialsEmail.ejs'),
       { name, email, password }
     );
 
-    // Send email via Resend API
     const message = await resend.emails.send({
-     from: "LeaveFlow Admin <ankeetkumar7777@gmail.com>",
+from: "LeaveFlow Admin <onboarding@resend.dev>",
+   // verified
       to: email,
       subject: "Your LeaveFlow Login Credentials",
       html: htmlContent,
     });
 
-    console.log("✅ Email sent successfully:", message.id,email);
+    console.log("✅ Email response:", message); // log full response
   } catch (err) {
     console.error("❌ Email sending failed:", err);
     throw err;
